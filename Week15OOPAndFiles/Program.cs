8850df91f9901530764528b924ced7d96bf40fed
Mishael <mishael.hussain@tktk.ee>
2023-04-24 22:03:12 +0300

    add Week15OOPAndFiles code

---
+++ Week15OOPAndFiles/Program.cs
@@ -0,0 +1,35 @@
+﻿
+string[] data = GetDataFromMyFile(); 
+ReadDataFromArray(data);
+
+
+foreach(string line in data)
+{
+    string[] tempArray = line.Split(new char[] { ';'}, StringSplitOptions.RemoveEmptyEntries);
+}
+static string[] GetDataFromMyFile()
+{
+    string filePath = @"C:\Users\37255\Downloads\movies.txt";
+    return File.ReadAllLines(filePath);
+
+} 
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach(string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+} 
+
+class Movie
+{
+    string title;
+    string year; 
+
+    public Movie(string _title, string _year)
+    {
+        title = title; 
+        year = _year;
+    }
+}
